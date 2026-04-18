1cdb98e2d9db992fa991c29495775ecafab32705
Andrey <andreiabdrahmanow@gmail.com>
2026-04-19 01:39:58 +0500

    Init

---
+++ ez.py
@@ -0,0 +1,35 @@
+values = [479.8,
+480.2,
+480.8,
+481.4,
+482.0,
+482.7,
+483.6,
+485.6,
+488.7,
+501.0
+]
+
+print()
+print('768')
+
+for i in range(len(values)):
+    print(values[i] / 768)
+    
+print()
+print('256')
+    
+for i in range(len(values)):
+    print(values[i] / 256)
+    
+print()
+print('32')
+    
+for i in range(len(values)):
+    print(values[i] / 32)
+    
+print()
+print('768')
+    
+for i in range(len(values)):
+    print(values[i] / 768)
